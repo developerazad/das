029df9105773b8fb2f9311a7060e71140191b0f0
developerazad <developerazad.hstu@gmail.com>
2019-06-10 23:55:19 +0600

    first commit in das

---
+++ footer.php
@@ -0,0 +1,31 @@
+   <!-- footer section --> 
+			<footer>
+				<div class="col-md-12 fsection">
+					<div class="col-md-4" id="contact">
+					<p>Category</p> <hr>
+					<ul>
+						<li><a href="signin.php">Search for Doctors</a></li>
+						<li><a href="signin.php">Contact with Doctors</a></li>
+						
+					</ul>
+					</div>
+					<div class="col-md-4">
+						<p>Contact</p> <hr>
+						<p>Md. Azharul Islam <br>
+							developerazad.hstu@gmail.com <br>
+							Cell: +8801764761919</p>
+							<span style="color: red;font-size: 15px">&copy;<?php echo date('Y'); ?>-All Rights Reserved</span>
+					</div>
+					<div class="col-md-4 share_img">
+					<p>Share my project</p> <hr>
+						<a href="http://www.facebook.com/sharer.php?u=http://portfolio.developerazad.com" target="_blank"><img src="img/fb-free.png" alt="facebok"></a>
+						<a href="https://plus.google.com/share?url=http:/portfolio.developerazad.com" target="_blank"><img src="img/gogle-plud-free.png" alt="google-plus"></a>
+						<a href="http://twitter.com/share?url=http://portfolio.developerazad.com" target="_blank"><img src="img/twitter.png" alt="twitter"></a>
+						
+					</div>
+				</div>
+				
+
+			</footer>
+
+		<!-- footer section Ends--> 
---
+++ patient/footer.php
@@ -0,0 +1,29 @@
+   <!-- footer section --> 
+			<footer>
+				<div class="col-md-12 fsection">
+					<div class="col-md-4" id="contact">
+					<p>Category</p>
+					<ul>
+						<li><a href="#">SignUp as a Donor</a></li>
+						<li><a href="#">Contact with Doctors</a></li>
+						
+					</ul>
+					</div>
+					<div class="col-md-4">
+						<p>Contact</p>
+						<p>Md. Azharul Islam <br>
+							BSc in ECE, HSTU, Dinajpur,</p>
+						
+					</div>
+					<div class="col-md-4 share_img">
+					<p>Share my website</p>
+						<a href="http://www.facebook.com/sharer.php?u=http://developerazad.wordpress.com" target="_blank"><img src="img/fb.png" alt=""></a>
+						<a href="https://plus.google.com/share?url=http://developerazad.wordpress.com" target="_blank"><img src="img/feed.png" alt=""></a>
+						<a href="http://twitter.com/share?url=http://developerazad.wordpress.com" target="_blank"><img src="img/twiter.png" alt=""></a>
+					</div>
+				</div>
+				
+
+			</footer>
+
+		<!-- footer section Ends--> 
